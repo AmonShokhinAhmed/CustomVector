2b56e8673d1b75b13dc95811ee0292b908d90c53
Amon Shokhin Ahmed <amon.shokhin.ahmed@gmail.com>
2021-03-02 23:12:11 +0100

    Implement push_back, reserve (and some other smaller functions)

--- CustomVector/CustomVector.h
+++ CustomVector/CustomVector.h
@@ -1,36 +1,80 @@
 #include "CustomIterator.h"
 template <typename T>
 class CustomVector {
+private:
+	T* _data; 
+	//TODO: maybe don't hard code this and instead actually find out the correct value
+	const unsigned int _maxSize = 10e8;
+	unsigned int _size;
+	unsigned int _capacity;
 public:
-	CustomVector() {
+	CustomVector(): _data(nullptr), _capacity(0), _size(0) {
+	}
 
+	~CustomVector() {
+		std::cout << "Vector Detor" << std::endl;
+		for (unsigned int i = 0; i < _size; i++) {
+			_data[i].~T();
+		}
 	}
-	T& at(unsigned index) {
-		T t = T();
-		return t;
+
+	T& at(const unsigned int index) {
+		if (index >= _size) {
+			//TODO: throw error
+		}
+		return _data[index];
 	}
+
 	T& operator[](const unsigned index) {
-		T t = T();
-		return t;
+		return _data[index];
 	}
+	
 	void push_back(const T& val) {
+		unsigned int newSize = _size + 1;
+		if (newSize > _maxSize) {
+			//TODO: throw error maybe
+			return;
+		}
+		if (newSize > _capacity) {
+			unsigned int newCapacity = _size + (_size / 2);
+			newCapacity = newCapacity >= newSize ? newCapacity : newSize;
+			reserve(newCapacity);
+		}
+		_data[_size] = T(val);
+		_size = newSize;
+	}
+	const unsigned int size(void) {
+		return _size;
+	}
+	const unsigned int max_size(void) {
+		return _maxSize;
+	}
+	const unsigned int capacity(void) {
+		return _capacity;
+	}
+	
+	void resize(const unsigned int n) {
+	 
+	}
+
+	void resize(const unsigned int n, const T& val) {
 
 	}
-	const size_t size(void) {
-		return 0;
-	}
-	const size_t capacity(void) {
-		return 0;
-	}
-	void resize(const unsigned n) {
 
+	void reserve(unsigned int n) {
+		if (n <= _capacity) {
+			return;
+		}
+		T* newData = reinterpret_cast<T*>(new char[sizeof(T) * n]);
+		for (unsigned int i = 0; i < _size; i++) {
+			newData[i] = _data[i];
+		}
+		T* oldData = _data;
+		_data = newData;
+		delete[] reinterpret_cast<char*>(oldData);
+		_capacity = n;
 	}
-	void resize(const unsigned n, const T& val) {
 
-	}
-	void reserve(unsigned n) {
-
-	}
 	CustomIterator begin(void) {
 		return CustomIterator();
 	}
--- CustomVector/main.cpp
+++ CustomVector/main.cpp
@@ -11,10 +11,27 @@
 	using vector = std::vector<T>;
 #endif
 
+	class Foo
+	{
+	public:
+		int data; 
+		Foo(int data):data(data) {
+			std::cout << "Constructor" << std::endl;
+		}
+		Foo(const Foo &foo):data(foo.data){ 
+			std::cout << "Copy Constructor" << std::endl; 
+		}
+		~Foo() {
+			std::cout << "Destructor" << std::endl;
+		}
+	};
 int main() {
 	vector<int> intVector;
+	std::cout << intVector.capacity() << std::endl;
+	std::cout << intVector.max_size() << std::endl;
 	for (unsigned i = 0; i < 10; ++i) {
 		intVector.push_back(i);
+		std::cout << intVector.capacity() << std::endl;
 	}
 	std::cout << "Size: " << intVector.size() <<std::endl;
 	std::cout << "At 2: " << intVector.at(2) << std::endl;
@@ -28,5 +45,23 @@ int main() {
 	intVector.reserve(24);
 	std::cout << "Capacity before reserve(24): " << intVector.capacity() << std::endl;
 
-
+	
+	vector<Foo>* fooVector = new vector<Foo>();
+	for (int i = 0; i < 10; ++i) {
+		fooVector->push_back(Foo(i));
+	}
+	vector<Foo>* secondFooVector = new vector<Foo>();
+	for (int i = 0; i < fooVector->size(); ++i) {
+		secondFooVector->push_back(fooVector->at(i));
+	}
+	for (int i = 0; i < 10; ++i) {
+		std::cout << &fooVector->at(i) << std::endl;
+		std::cout << &secondFooVector->at(i) << std::endl;
+	}
+	delete fooVector;
+	for (int i = 0; i < secondFooVector->size(); ++i) {
+		std::cout << secondFooVector->at(i).data << std::endl;
+	}
+	delete secondFooVector;
+	
 }
