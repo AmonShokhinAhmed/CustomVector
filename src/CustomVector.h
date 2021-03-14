a8a437484a314616d08fc8be113e709a5ed6f227
AmonShokhinAhmed <amon.shokhin.ahmed@gmail.com>
2021-03-14 13:59:20 +0100

    Add necesssary compile flags, and fix warnings

--- src/CustomVector.h
+++ src/CustomVector.h
@@ -1,168 +1,165 @@
 #include <cassert>
-template <typename T>
-class CustomVector {
+
+#define MAXSIZE 10e8
+
+template <typename T> class CustomVector {
 private:
-	T* _data; 
-	//TODO: maybe don't hard code this and instead actually find out the correct value
-	const uint32_t _maxSize = 10e8;
-	uint32_t _size;
-	uint32_t _capacity;
+  T *_data;
+  // TODO: maybe don't hard code this and instead actually find out the correct
+  // value
+  const uint32_t _maxSize;
+  uint32_t _size;
+  uint32_t _capacity;
+
 public:
-	struct Iterator {
-		using iterator_category = std::forward_iterator_tag;
-		using difference_type = std::ptrdiff_t;
-		using value_type = T;
-		using pointer = T*;  // or also value_type*
-		using reference = T&;  // or also value_type&
+  struct Iterator {
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = T;
+    using pointer = T *;   // or also value_type*
+    using reference = T &; // or also value_type&
 
-		Iterator(pointer ptr): m_ptr(ptr) {}
-		reference operator*() const { return *m_ptr; }
-		pointer operator->() { return m_ptr; }
-		Iterator& operator++() { m_ptr++; return *this; }
-		Iterator operator++(int) { Iterator tmp = *this; ++(*this); return tmp; }
-		friend bool operator== (const Iterator& a, const Iterator& b) { return a.m_ptr == b.m_ptr; };
-		friend bool operator!= (const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; };
-		Iterator operator+(const int& i) {
-			Iterator iterator(*this);
-			for (int j = 0; j < i; j++) {
-				iterator++;
-			}
-			return iterator;
-		}
-	private:
-		T* m_ptr;
-	};
-	CustomVector(): _data(nullptr), _capacity(0), _size(0) {
-	}
-	~CustomVector() {
-		for (uint32_t i = 0; i < _size; i++) {
-			_data[i].~T();
-		}
-		delete[] reinterpret_cast<char*>(_data);
-	}
+    Iterator(pointer ptr) : m_ptr(ptr) {}
+    reference operator*() const { return *m_ptr; }
+    pointer operator->() { return m_ptr; }
+    Iterator &operator++() {
+      m_ptr++;
+      return *this;
+    }
+    Iterator operator++(int) {
+      Iterator tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+    friend bool operator==(const Iterator &a, const Iterator &b) {
+      return a.m_ptr == b.m_ptr;
+    };
+    friend bool operator!=(const Iterator &a, const Iterator &b) {
+      return a.m_ptr != b.m_ptr;
+    };
+    Iterator operator+(const int &i) {
+      Iterator iterator(*this);
+      for (int j = 0; j < i; j++) {
+        iterator++;
+      }
+      return iterator;
+    }
 
-	T& at(const uint32_t index) {
-		assert(index >= 0 && index < _size);
-		return _data[index];
-	}
-	T& operator[](const uint32_t index) {
-		return _data[index];
-	}
-	
-	void push_back(const T& val) {
-		uint32_t newSize = _size + 1;
-		assert(newSize<=_maxSize);
-		if (newSize > _capacity) {
-			uint32_t newCapacity = _size + (_size / 2);
-			newCapacity = newCapacity >= newSize ? newCapacity : newSize;
-			reserve(newCapacity);
-		}
-		_data[_size] = T(val);
-		_size = newSize;
-	}
+  private:
+    T *m_ptr;
+  };
+  CustomVector() : _data(nullptr), _maxSize(MAXSIZE), _size(0), _capacity(0) {}
+  ~CustomVector() {
+    for (uint32_t i = 0; i < _size; i++) {
+      _data[i].~T();
+    }
+    delete[] reinterpret_cast<char *>(_data);
+  }
 
-	const uint32_t size(void) {
-		return _size;
-	}
-	const uint32_t max_size(void) {
-		return _maxSize;
-	}
-	const uint32_t capacity(void) {
-		return _capacity;
-	}
-	
-	void resize(const uint32_t n) {
-		resize(n, T());
-	}
-	void resize(const uint32_t n, const T& val) {
-		if (n > _maxSize) {
-			//TODO: throw error maybe
-			return;
-		}
+  T &at(const uint32_t index) {
+    assert(index >= 0 && index < _size);
+    return _data[index];
+  }
+  T &operator[](const uint32_t index) { return _data[index]; }
 
-		uint32_t newCapacity = _capacity;
-		while (newCapacity < n) {
-			newCapacity += (newCapacity / 2);
-		}
-		T* newData = reinterpret_cast<T*>(new char[sizeof(T) * newCapacity]);
-		if (n <= _size) {
-			for (int i = 0; i < n; i++) {
-				newData[i] = _data[i];
-				_data[i].~T();
-			}
-		}
-		else {
-			for (int i = 0; i < _size; i++) {
-				newData[i] = _data[i];
-				_data[i].~T();
-			}
-			for (int i = _size; i < n; i++) {
-				//This will throw an error if no default constructor is provided, but std::vector does as well
-				newData[i] = T(val);
-			}
-		}
-		_capacity = newCapacity;
-		_size = n;
-		T* oldData = _data;
-		_data = newData;
-		delete[] reinterpret_cast<char*>(oldData);
-	}
-	void reserve(const uint32_t n) {
-		if (n <= _capacity) {
-			return;
-		}
-		T* newData = reinterpret_cast<T*>(new char[sizeof(T) * n]);
-		for (uint32_t i = 0; i < _size; i++) {
-			newData[i] = _data[i];
-			_data[i].~T();
-		}
-		T* oldData = _data;
-		_data = newData;
-		delete[] reinterpret_cast<char*>(oldData);
-		_capacity = n;
-	}
+  void push_back(const T &val) {
+    uint32_t newSize = _size + 1;
+    assert(newSize <= _maxSize);
+    if (newSize > _capacity) {
+      uint32_t newCapacity = _size + (_size / 2);
+      newCapacity = newCapacity >= newSize ? newCapacity : newSize;
+      reserve(newCapacity);
+    }
+    _data[_size] = T(val);
+    _size = newSize;
+  }
 
-	const T* as_array(void) {
-		return _data;
-	}
-	void erase_by_swap(const uint32_t n) {
-		assert(n < _size);
-		_size--;
-		if (n != _size) {
-			_data[n].~T();
-			_data[n] = _data[_size];
-		}
-		_data[_size].~T();
-	}
+  uint32_t size(void) { return _size; }
+  uint32_t max_size(void) { return _maxSize; }
+  uint32_t capacity(void) { return _capacity; }
 
-	Iterator begin() {
-		return Iterator(&_data[0]);
-	}
-	Iterator end() {
-		return Iterator(&_data[_size]);
-	}
-	Iterator erase(Iterator position) {
-		return erase(position, position);
-	}
-	Iterator erase(Iterator first, Iterator last) {
-		if (first == end()) {
-			return first;
-		}
-		Iterator curPosition = first;
-		Iterator nextPosition = last + 1;
-		while (nextPosition != end()) {
-			(*curPosition).~T();
-			(*curPosition) = (*(nextPosition));
-			++curPosition;
-			++nextPosition;
-		}
-		uint32_t delAmount = 0;
-		while (curPosition != end()) {
-			(*curPosition).~T();
-			++curPosition;
-			delAmount++;
-		}
-		_size -= delAmount;
-		return first;
-	}
-}; 
+  void resize(const uint32_t n) { resize(n, T()); }
+  void resize(const uint32_t n, const T &val) {
+    if (n > _maxSize) {
+      // TODO: throw error maybe
+      return;
+    }
+
+    uint32_t newCapacity = _capacity;
+    while (newCapacity < n) {
+      newCapacity += (newCapacity / 2);
+    }
+    T *newData = reinterpret_cast<T *>(new char[sizeof(T) * newCapacity]);
+    if (n <= _size) {
+      for (uint32_t i = 0; i < n; i++) {
+        newData[i] = _data[i];
+        _data[i].~T();
+      }
+    } else {
+      for (uint32_t i = 0; i < _size; i++) {
+        newData[i] = _data[i];
+        _data[i].~T();
+      }
+      for (uint32_t i = _size; i < n; i++) {
+        // This will throw an error if no default constructor is provided, but
+        // std::vector does as well
+        newData[i] = T(val);
+      }
+    }
+    _capacity = newCapacity;
+    _size = n;
+    T *oldData = _data;
+    _data = newData;
+    delete[] reinterpret_cast<char *>(oldData);
+  }
+  void reserve(const uint32_t n) {
+    if (n <= _capacity) {
+      return;
+    }
+    T *newData = reinterpret_cast<T *>(new char[sizeof(T) * n]);
+    for (uint32_t i = 0; i < _size; i++) {
+      newData[i] = _data[i];
+      _data[i].~T();
+    }
+    T *oldData = _data;
+    _data = newData;
+    delete[] reinterpret_cast<char *>(oldData);
+    _capacity = n;
+  }
+
+  const T *as_array(void) { return _data; }
+  void erase_by_swap(const uint32_t n) {
+    assert(n < _size);
+    _size--;
+    if (n != _size) {
+      _data[n].~T();
+      _data[n] = _data[_size];
+    }
+    _data[_size].~T();
+  }
+
+  Iterator begin() { return Iterator(&_data[0]); }
+  Iterator end() { return Iterator(&_data[_size]); }
+  Iterator erase(Iterator position) { return erase(position, position); }
+  Iterator erase(Iterator first, Iterator last) {
+    if (first == end()) {
+      return first;
+    }
+    Iterator curPosition = first;
+    Iterator nextPosition = last + 1;
+    while (nextPosition != end()) {
+      (*curPosition).~T();
+      (*curPosition) = (*(nextPosition));
+      ++curPosition;
+      ++nextPosition;
+    }
+    uint32_t delAmount = 0;
+    while (curPosition != end()) {
+      (*curPosition).~T();
+      ++curPosition;
+      delAmount++;
+    }
+    _size -= delAmount;
+    return first;
+  }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,107 +3,112 @@
 #include <iostream>
 #ifdef CUSTOMVECTOR
 #include "CustomVector.h"
-template <typename T>
-using vector = CustomVector<T>;
+template <typename T> using vector = CustomVector<T>;
 #else
 #include <vector>
-template <typename T>
-using vector = std::vector<T>;
+template <typename T> using vector = std::vector<T>;
 #endif
 
-class Foo
-{
+class Foo {
 public:
-	static int fooCount;
-	int data;
-	Foo(int data) :data(data) {
-		//std::cout << "Constructor" << std::endl;
-		fooCount++;
-	}
-	Foo& operator=(const Foo& foo) {
-		//std::cout << "Copy Assignment" << std::endl;
-		data = foo.data;
-		fooCount ++;
-		return *this;
-	}
-	Foo(const Foo& foo) :data(foo.data) {
-		//std::cout << "Copy" << std::endl;
-		fooCount++;
-	}
-	~Foo() {
-		//std::cout << "Destructor" << std::endl;
-		fooCount--;
-	}
+  static int fooCount;
+  int data;
+  Foo(int data) : data(data) {
+    // std::cout << "Constructor" << std::endl;
+    fooCount++;
+  }
+  Foo &operator=(const Foo &foo) {
+    // std::cout << "Copy Assignment" << std::endl;
+    data = foo.data;
+    fooCount++;
+    return *this;
+  }
+  Foo(const Foo &foo) : data(foo.data) {
+    // std::cout << "Copy" << std::endl;
+    fooCount++;
+  }
+  ~Foo() {
+    // std::cout << "Destructor" << std::endl;
+    fooCount--;
+  }
 };
 int Foo::fooCount = 0;
 int main() {
-	/*
-	vector<int> intVector;
-	std::cout << intVector.capacity() << std::endl;
-	for (uint32_t i = 0; i < 10; ++i) {
-		intVector.push_back(i);
-		std::cout << intVector.capacity() << std::endl;
-	}
-	std::cout << "Size: " << intVector.size() << std::endl;
-	std::cout << "At 2: " << intVector.at(2) << std::endl;
-	std::cout << "[]: " << intVector[2] << std::endl;
-	//intVector.erase(intVector.begin()+2);
-	//std::cout << "Size after erase: "<< intVector.size() << std::endl;
-	//std::cout << "At 2 after erase: " << intVector.at(2) << std::endl;
-	intVector.resize(5);
-	std::cout << "Size after resize(5): " << intVector.size() << std::endl;
-	std::cout << "Capacity after resize(5): " << intVector.capacity() << std::endl;
-	intVector.resize(15);
-	std::cout << "Size after resize(5): " << intVector.size() << std::endl;
-	std::cout << "Capacity after resize(15): " << intVector.capacity() << std::endl;
-	std::cout << intVector.at(14) << std::endl;
-	intVector.resize(20, 2);
-	std::cout << "Size after resize(5): " << intVector.size() << std::endl;
-	std::cout << "Capacity after resize(20): " << intVector.capacity() << std::endl;
-	intVector.reserve(48);
-	std::cout << "Capacity after reserve(48): " << intVector.capacity() << std::endl;
-	*/
-	{
-	vector<Foo> fooVector = vector<Foo>();
-	for (int i = 0; i < 10; ++i) {
-		Foo foo = Foo(i);
-		fooVector.push_back(foo);
-	}
-	for (auto i = fooVector.begin(); i!=fooVector.end(); i++){
-		std::cout << "iterator loop at: " << (*i).data << std::endl ;
-	}
-	std::cout << "size before erase at: " << fooVector.size() << std::endl << std::endl;
+  /*
+  vector<int> intVector;
+  std::cout << intVector.capacity() << std::endl;
+  for (uint32_t i = 0; i < 10; ++i) {
+          intVector.push_back(i);
+          std::cout << intVector.capacity() << std::endl;
+  }
+  std::cout << "Size: " << intVector.size() << std::endl;
+  std::cout << "At 2: " << intVector.at(2) << std::endl;
+  std::cout << "[]: " << intVector[2] << std::endl;
+  //intVector.erase(intVector.begin()+2);
+  //std::cout << "Size after erase: "<< intVector.size() << std::endl;
+  //std::cout << "At 2 after erase: " << intVector.at(2) << std::endl;
+  intVector.resize(5);
+  std::cout << "Size after resize(5): " << intVector.size() << std::endl;
+  std::cout << "Capacity after resize(5): " << intVector.capacity() <<
+  std::endl; intVector.resize(15); std::cout << "Size after resize(5): " <<
+  intVector.size() << std::endl; std::cout << "Capacity after resize(15): " <<
+  intVector.capacity() << std::endl; std::cout << intVector.at(14) << std::endl;
+  intVector.resize(20, 2);
+  std::cout << "Size after resize(5): " << intVector.size() << std::endl;
+  std::cout << "Capacity after resize(20): " << intVector.capacity() <<
+  std::endl; intVector.reserve(48); std::cout << "Capacity after reserve(48): "
+  << intVector.capacity() << std::endl;
+  */
+  {
+    vector<Foo> fooVector = vector<Foo>();
+    for (int i = 0; i < 10; ++i) {
+      Foo foo = Foo(i);
+      fooVector.push_back(foo);
+    }
+    for (auto i = fooVector.begin(); i != fooVector.end(); i++) {
+      std::cout << "iterator loop at: " << (*i).data << std::endl;
+    }
+    std::cout << "size before erase at: " << fooVector.size() << std::endl
+              << std::endl;
 
-	fooVector.erase(fooVector.begin() + 2, fooVector.begin()+5);
-	for (auto i = fooVector.begin(); i != fooVector.end(); i++) {
-		std::cout << "iterator loop after erase at: " << (*i).data << std::endl;
-	}
-	std::cout << "size after erase at: " << fooVector.size() << std::endl << std::endl;
+    fooVector.erase(fooVector.begin() + 2, fooVector.begin() + 5);
+    for (auto i = fooVector.begin(); i != fooVector.end(); i++) {
+      std::cout << "iterator loop after erase at: " << (*i).data << std::endl;
+    }
+    std::cout << "size after erase at: " << fooVector.size() << std::endl
+              << std::endl;
 
-	fooVector.erase(fooVector.begin() + 2);
-	for (auto i = fooVector.begin(); i != fooVector.end(); i++) {
-		std::cout << "iterator loop after erase at: " << (*i).data << std::endl;
-	}
-	std::cout << "size after erase at: " << fooVector.size() << std::endl << std::endl;
+    fooVector.erase(fooVector.begin() + 2);
+    for (auto i = fooVector.begin(); i != fooVector.end(); i++) {
+      std::cout << "iterator loop after erase at: " << (*i).data << std::endl;
+    }
+    std::cout << "size after erase at: " << fooVector.size() << std::endl
+              << std::endl;
 
-	std::cout << std::endl << "fooVector->size(): " << fooVector.size() << " Foocount: " << Foo::fooCount << std::endl;
-	std::cout << std::endl << "fooVector->resize(20, Foo(1));" << std::endl;
-	fooVector.resize(20, Foo(1));
+    std::cout << std::endl
+              << "fooVector->size(): " << fooVector.size()
+              << " Foocount: " << Foo::fooCount << std::endl;
+    std::cout << std::endl << "fooVector->resize(20, Foo(1));" << std::endl;
+    fooVector.resize(20, Foo(1));
 
-	std::cout << std::endl << "fooVector->size(): " << fooVector.size() << " Foocount: " << Foo::fooCount << std::endl;
+    std::cout << std::endl
+              << "fooVector->size(): " << fooVector.size()
+              << " Foocount: " << Foo::fooCount << std::endl;
 
-	std::cout << std::endl << "vector<Foo>* secondFooVector = new vector<Foo>();" << std::endl;
-	vector<Foo> secondFooVector = vector<Foo>();
+    std::cout << std::endl
+              << "vector<Foo>* secondFooVector = new vector<Foo>();"
+              << std::endl;
+    vector<Foo> secondFooVector = vector<Foo>();
 
-	for (int i = 0; i < fooVector.size(); ++i) {
-		secondFooVector.push_back(fooVector.at(i));
-	}
-	fooVector.erase_by_swap(2);
-	//fooVector.erase(fooVector.begin() + 2);
-	std::cout << std::endl << "for loop" << std::endl << std::endl;
-	for (auto f : fooVector) {
-		std::cout << f.data << std::endl;
-	}
-}
-	std::cout << "Foocount: " << Foo::fooCount << std::endl;
+    for (uint32_t i = 0; i < fooVector.size(); ++i) {
+      secondFooVector.push_back(fooVector.at(i));
+    }
+    fooVector.erase_by_swap(2);
+    // fooVector.erase(fooVector.begin() + 2);
+    std::cout << std::endl << "for loop" << std::endl << std::endl;
+    for (auto f : fooVector) {
+      std::cout << f.data << std::endl;
+    }
+  }
+  std::cout << "Foocount: " << Foo::fooCount << std::endl;
 }
